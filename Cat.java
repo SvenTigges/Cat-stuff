910a6b4b4d5b32e3d31c8387bb95946e2f4d2a1f
SvenTigges <sven.tigges@web.de>
2021-06-02 14:56:19 +0200

    01_Refactoring App/Cat/Tomcat

--- Cat.java
+++ Cat.java
@@ -4,14 +4,14 @@ public class Cat
     private String name;
     private String furColor;
     private int age;
-    private boolean isFemale;
+    
     private int level; // intern verwendet
 
-    public Cat(String name, String furColor, int age, boolean isFemale) {
+    public Cat(String name, String furColor, int age) {
         this.name = name;
         this.furColor = furColor;
         this.age = age;
-        this.isFemale = isFemale;
+        
     }
 
     public String tellYourAttributes(String checkStr) {
@@ -20,29 +20,18 @@ public class Cat
                 return this.name;
             case "#color":
                 return this.furColor;
-            case "#age":
-                return this.checkCompliance();
             default:
                 return "ERROR";
         }
     }
 
-    private String checkCompliance() {
-        if (isFemale) {
-            return this.checkEscalationLevel();
-        } else {
-            return Integer.toString(this.age);
-        }
+    public String tellYourAge() {
+        return this.checkEscalationLevel();
     }
 
     private String checkEscalationLevel() {
 
-        // 1. --> "This is an inappropriate question!"
-		// 2. --> "I've told you once!"
-		// 3. --> "Talk to the hand!"
-		// 4++. --> "#!?&&%"
-
-         this.level++;
+        this.level++;
 
         switch (level) {
             case 1:
@@ -56,5 +45,4 @@ public class Cat
         }
 
     }
-
-}
+}       
